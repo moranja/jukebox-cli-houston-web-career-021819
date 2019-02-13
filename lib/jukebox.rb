f5fe7ff7ebafbec069f57d80ff5360b016e241a4
Adam Moran <jeffadammoran@gmail.com>
2019-02-13 21:14:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,25 +55,25 @@ def exit_jukebox
   puts "Goodbye"
 end
 
-# def run(song_array)
-#   help
-#   command = "test"
-#   while command != "exit" 
+def run(song_array)
+  help
+  command = "test"
+  while command != "exit" 
   
-#   puts "Please enter a command:"
-#   command = gets.chomp 
+  puts "Please enter a command:"
+  command = gets.chomp 
   
-#     if command == "help"
-#       help
-#     elsif command == "list"
-#       list (song_array)
-#     elsif command == "play"
-#       play (song_array)
-#     elsif command == "exit"
-#       exit_jukebox
-#     end
-#   end
-# end
-# end
+    if command == "help"
+      help
+    elsif command == "list"
+      list (song_array)
+    elsif command == "play"
+      play (song_array)
+    elsif command == "exit"
+      exit_jukebox
+    end
+  end
+end
+end
   
   

84b0ce5be1d6684ac3c45f1ad15d0e1a6847860a
Adam Moran <jeffadammoran@gmail.com>
2019-02-13 21:17:31 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,70 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list (songs)
+  songs.each_index {|i| puts "#{i+1}. #{songs[i]}"}
+  
+  #i = 0
+  #while songs[i] <= songs.length 
+  #  puts "#{i+1}. #{songs[i]}"
+  #  i += 1
+  #end
+  
+end
+
+def play (songs)
+  puts "Please enter a song name or number:"
+  song_name_or_number = gets.chomp
+  if song_name_or_number.to_i.to_s == song_name_or_number
+    if song_name_or_number.to_i <= songs.length
+      correct_number = song_name_or_number.to_i - 1
+      #binding.pry
+      puts "Playing #{songs[correct_number]}"
+    else
+      puts "Invalid input, please try again"
+    end
+  elsif 
+    songs.each do |i|
+      if i == song_name_or_number
+        puts "Playing #{i}"
+        #break
+      end
+    end
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(song_array)
+  help
+  command = "test"
+  while command != "exit" 
+  
+  puts "Please enter a command:"
+  command = gets.chomp 
+  
+    if command == "help"
+      help
+    elsif command == "list"
+      list (song_array)
+    elsif command == "play"
+      play (song_array)
+    elsif command == "exit"
+      exit_jukebox
+    end
+  end
+end
+  
+  
